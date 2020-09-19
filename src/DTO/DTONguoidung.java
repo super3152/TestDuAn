4e1d88a2fb04e557844939e6d5ff7b23a9ac447e
Administrator <Administrator@21AK22-COM>
2020-09-19 19:59:10 +0700

    nguyendaumoi

--- src/DTO/DTONguoidung.java
+++ src/DTO/DTONguoidung.java
@@ -5,10 +5,108 @@
  */
 package DTO;
 
+import java.util.Date;
+
 /**
  *
  * @author Administrator
  */
 public class DTONguoidung {
+    int IdNguoiDung;
+    String TenNguoiDung;
+    String GioiTinh;
+    Date NgaySinh;
+    Date NgayVaoLam;
+    int CMND;
+    String TenDangNhap;
+    String MatKhau;
+    int Quyen;
+
+    public DTONguoidung() {
+    }
+
+    public DTONguoidung(int IdNguoiDung, String TenNguoiDung, String GioiTinh, Date NgaySinh, Date NgayVaoLam, int CMND, String TenDangNhap, String MatKhau, int Quyen) {
+        this.IdNguoiDung = IdNguoiDung;
+        this.TenNguoiDung = TenNguoiDung;
+        this.GioiTinh = GioiTinh;
+        this.NgaySinh = NgaySinh;
+        this.NgayVaoLam = NgayVaoLam;
+        this.CMND = CMND;
+        this.TenDangNhap = TenDangNhap;
+        this.MatKhau = MatKhau;
+        this.Quyen = Quyen;
+    }
+
+    public int getIdNguoiDung() {
+        return IdNguoiDung;
+    }
+
+    public void setIdNguoiDung(int IdNguoiDung) {
+        this.IdNguoiDung = IdNguoiDung;
+    }
+
+    public String getTenNguoiDung() {
+        return TenNguoiDung;
+    }
+
+    public void setTenNguoiDung(String TenNguoiDung) {
+        this.TenNguoiDung = TenNguoiDung;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public Date getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public void setNgaySinh(Date NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public Date getNgayVaoLam() {
+        return NgayVaoLam;
+    }
+
+    public void setNgayVaoLam(Date NgayVaoLam) {
+        this.NgayVaoLam = NgayVaoLam;
+    }
+
+    public int getCMND() {
+        return CMND;
+    }
+
+    public void setCMND(int CMND) {
+        this.CMND = CMND;
+    }
+
+    public String getTenDangNhap() {
+        return TenDangNhap;
+    }
+
+    public void setTenDangNhap(String TenDangNhap) {
+        this.TenDangNhap = TenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String MatKhau) {
+        this.MatKhau = MatKhau;
+    }
+
+    public int getQuyen() {
+        return Quyen;
+    }
+
+    public void setQuyen(int Quyen) {
+        this.Quyen = Quyen;
+    }
     
 }
